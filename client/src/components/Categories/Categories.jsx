ac4b32a4a19043560bc50042c8a7d82cbeb1be20
irynit <irynachmel@gmail.com>
2023-03-06 16:26:36 +0200

    fixed caterories layout

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -10,16 +10,18 @@ export default function Categories() {
         <div className="categories__col">
           <div className="categories__col-row">3</div>
         </div>
-        <div className="categories__col-l">
-          <div className="categories__col-l-row">
-            <div className="categories__col-l">
-              <div className="categories__col-l-row">4</div>
+        <div className="categories__col-large">
+          <div className="categories__col-large-top">
+            <div className="categories__col">
+              <div className="categories__col-row">4</div>
             </div>
             <div className="categories__col">
-              <div className="categories__col-row">5</div>
+              <div className="categories__col-row">6</div>
             </div>
           </div>
-          <div className="categories__col-l-row">6</div>
+          <div className="categories__col-large-bottom">
+            <div className="categories__col-row">5</div>
+          </div>
         </div>
       </div>
     </section>
