ec1f895af5f020e0b97993e0e9c85451b3a99600
irynit <irynachmel@gmail.com>
2023-03-08 21:19:35 +0200

    product end

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Product.scss";
 import { useState } from "react";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 export default function Product() {
-  const [selectedImg,setSelectedImg] = useState(0)
-  const images = ["https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-"https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"];
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setOuantity] = useState(1);
+  const images = [
+    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  ];
   return (
     <section className="product">
       <div className="container product__container">
@@ -14,11 +20,56 @@ export default function Product() {
             <img src={images[1]} alt="" nClick={(e) => setSelectedImg(1)} />
           </div>
           <div className="product__left-row">
-            <img src={images[setSelectedImg]} alt="" />
+            <img src={images[selectedImg]} alt="" />
           </div>
         </div>
-        <div className="product__right"></div>
-      </div>
+        < div className="product__right">
+          <h2 className="product__right-title">Title</h2>
+          <span>$199</span>
+          <p className="product__right-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quo
+            nisi totam assumenda asperiores, odit a voluptas aperiam velit
+            expedita necessitatibus aliquid accusamus quibusdam alias dolorum
+            beatae nam adipisci qui.
+          </p>
+          <div className="product__right-quantity">
+            <button
+              onClick={() => setOuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+            >
+              -
+            </button>
+            {quantity}
+            <button onClick={() => setOuantity((prev) => prev + 1)}>+</button>
+          </div>
+          <button className="product__right-add">
+            <AddShoppingCartIcon /> ADD TO CART
+          </button>
+          <div className="product__right-link">
+            <div className="product__right-link-item">
+              <FavoriteBorderIcon />
+              ADD TO WISH LIST
+            </div>
+            <div className="product__right-link-item">
+              <BalanceIcon /> ADD TO COMPARE
+            </div>
+          </div>
+          <div className="product__right-info">
+            <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="product__right-details">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
+          
+          </div>
+      
+    </div>
     </section>
   );
 }
