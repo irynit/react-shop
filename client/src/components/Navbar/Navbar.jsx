93e0b01d39bfa86ff8367e7ea3b75a1c43faeed4
irynit <irynachmel@gmail.com>
2023-03-13 21:11:34 +0200

    fixed: img single prod, navbar, font-family/size

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -17,61 +17,65 @@ export default function Navbar() {
       <div className="container header__container">
         <nav className="header__navbar">
           <div className="header__navbar-left">
-            <div className="item">
+            <div className="header__navbar-left-item">
               <img src={languageEn} alt="" />
               <KeyboardArrowDownIcon />
             </div>
-            <div className="item">
+            <div className="header__navbar-left-item">
               <span>USD</span>
               <KeyboardArrowDownIcon />
             </div>
-            <div className="item">
+            <div className="header__navbar-left-item">
               <Link className="link" to="/products1">
                 Women
               </Link>
             </div>
-            <div className="item">
+            <div className="header__navbar-left-item">
               <Link className="link" to="/products2">
                 Men
               </Link>
             </div>
-            <div className="item">
+            <div className="header__navbar-left-item">
               <Link className="link" to="/products3">
                 Children
               </Link>
             </div>
           </div>
+
           <div className="header__navbar-center">
             <Link className="link" to="/">
               LAMASTORE
             </Link>
           </div>
           <div className="header__navbar-right">
-            <div className="item">
+            <div className="header__navbar-right-item">
               <Link className="link" to="/">
                 Homepage
               </Link>
             </div>
-            <div className="item">
+            <div className="header__navbar-right-item">
               <Link className="link" to="/">
                 About
               </Link>
             </div>
-            <div className="item">
+            <div className="header__navbar-right-item">
               <Link className="link" to="/">
                 Contact
               </Link>
             </div>
-            <div className="item">
+            <div className="header__navbar-right-item">
               <Link className="link" to="/">
                 Stores
               </Link>
             </div>
-            <div className="icons">
+            <div className="header__navbar-right-icons">
               <SearchIcon />
               <PersonOutlinedIcon />
               <FavoriteBorderOutlinedIcon />
-              <div className="cartIcon" onClick={()=>setOpen(!open)}>
+              <div
+                className="header__navbar-right-icons-cart"
+                onClick={() => setOpen(!open)}
+              >
                 <ShoppingCartOutlinedIcon />
                 <span>0</span>
               </div>
