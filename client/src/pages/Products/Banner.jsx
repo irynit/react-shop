45521b366b49ecb0ab94d90bee7a5403a46822f7
irynit <irynachmel@gmail.com>
2023-03-21 11:31:31 +0200

    1

--- client/src/pages/Products/Banner.jsx
+++ client/src/pages/Products/Banner.jsx
@@ -1,16 +1,14 @@
-import useFetch from '../../hooks/useFetch';
+import useFetch from "../../hooks/useFetch";
 
 export default function Banner(props) {
   const { data, loading, error } = useFetch(
-    `/categories?populate=*&[filter][categories][id][$eq]=${props.catId}`
+    `/categories?populate=*&[filter][categories][id][$eq]=${props.categoryId}`
   );
 
   console.log(data);
   return (
     <div className={props.classAttrName}>
-      <img 
-      src={data?.attributes?.img?.data?.attributes?.url} 
-      alt="" />
+      <img src={data?.attributes?.img?.data?.attributes?.url} alt="" />
     </div>
   );
 }
